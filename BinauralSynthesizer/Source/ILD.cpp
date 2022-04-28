ee0e395085376fedb5d96a34c92c7834f9dd822b
mwilliams2099 <m.williams200799@gmail.com>
2022-04-28 09:04:27 +0100

    File opening improved

--- BinauralSynthesizer/Source/ILD.cpp
+++ BinauralSynthesizer/Source/ILD.cpp
@@ -31,86 +31,34 @@ float ILDClass::process(float sample, int channel)
 
 void ILDClass::loadAmplitudeDatabase()
 {
-    std::ifstream subAmplitudesLeftFile("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/subBandAmplitudes_left.txt");
-    std::ifstream subAmplitudesRightFile("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/subBandAmplitudes_right.txt");
-    std::ifstream lowAmplitudesLeftFile("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/lowBandAmplitudes_left.txt");
-    std::ifstream lowAmplitudesRightFile("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/lowBandAmplitudes_right.txt");
-    std::ifstream midAmplitudesLeftFile("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/midBandAmplitudes_left.txt");
-    std::ifstream midAmplitudesRightFile("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/midBandAmplitudes_right.txt");
-    std::ifstream highAmplitudesLeftFile("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/highBandAmplitudes_left.txt");
-    std::ifstream highAmplitudesRightFile("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/highBandAmplitudes_right.txt");
-    //if (!file_amps)
-     //   file_test = 666;
-    int n = 0; 
-    if (subAmplitudesLeftFile.is_open())
+    std::ifstream amplitudesFile[8]; ("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/subBandAmplitudes_left.txt");
+    amplitudesFile[0].open("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/subBandAmplitudes_left.txt");
+    amplitudesFile[1].open("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/subBandAmplitudes_right.txt");
+    amplitudesFile[2].open("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/lowBandAmplitudes_left.txt");
+    amplitudesFile[3].open("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/lowBandAmplitudes_right.txt");
+    amplitudesFile[4].open("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/midBandAmplitudes_left.txt");
+    amplitudesFile[5].open("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/midBandAmplitudes_right.txt");
+    amplitudesFile[6].open("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/highBandAmplitudes_left.txt");
+    amplitudesFile[7].open("C:/Users/Matthew Williams/Documents/JUCE Plugin Dev/BinauralSynthesizer/BinauralSynthesizer/Source/highBandAmplitudes_right.txt");
+
+    int m = 0;
+    int t = 0;
+    for (int i = 0; i < 8; i++)
     {
-        while (subAmplitudesLeftFile >> amplitudes[n][0][0])
+        int n = 0;
+        if (amplitudesFile[i].is_open())
         {
-            n++;
+            while (amplitudesFile[i] >> amplitudes[n][t][m])
+            {
+                n++;
+            }
+            amplitudesFile[i].close();
         }
-        subAmplitudesLeftFile.close();
-    }
-    n = 0;
-    if (subAmplitudesRightFile.is_open())
-    {
-        while (subAmplitudesRightFile >> amplitudes[n][0][1])
+        m++;
+        if (m == 2)
         {
-            n++;
+            t++;
+            m = 0;
         }
-        subAmplitudesRightFile.close();
-    }
-    n = 0;
-    if (lowAmplitudesLeftFile.is_open())
-    {
-        while (lowAmplitudesLeftFile >> amplitudes[n][1][0])
-        {
-            n++;
-        }
-        lowAmplitudesLeftFile.close();
-    }
-    n = 0;
-    if (lowAmplitudesRightFile.is_open())
-    {
-        while (lowAmplitudesRightFile >> amplitudes[n][1][1])
-        {
-            n++;
-        }
-        lowAmplitudesRightFile.close();
-    }
-    n = 0;
-    if (midAmplitudesLeftFile.is_open())
-    {
-        while (midAmplitudesLeftFile >> amplitudes[n][2][0])
-        {
-            n++;
-        }
-        midAmplitudesLeftFile.close();
-    }
-    n = 0;
-    if (midAmplitudesRightFile.is_open())
-    {
-        while (midAmplitudesRightFile >> amplitudes[n][2][1])
-        {
-            n++;
-        }
-        midAmplitudesRightFile.close();
-    }
-    n = 0;
-    if (highAmplitudesLeftFile.is_open())
-    {
-        while (highAmplitudesLeftFile >> amplitudes[n][3][0])
-        {
-            n++;
-        }
-        highAmplitudesLeftFile.close();
-    }
-    n = 0;
-    if (highAmplitudesRightFile.is_open())
-    {
-        while (highAmplitudesRightFile >> amplitudes[n][3][1])
-        {
-            n++;
-        }
-        highAmplitudesRightFile.close();
     }
 }
